6bc69f0cacf6c683a67e9b72b24d9c4d3dcc933d
noracami <kerke..2011@gmail.com>
2014-09-01 09:31:29 +0800

    update ex36.py

--- exercise/ex36.py
+++ exercise/ex36.py
@@ -3,8 +3,19 @@ from sys import exit
 #def gold_room()
 
 #def bear_room()
+def num_to_list(num):
+    output = []
+    
 
 #def cthulhu_room()
+def input_int_plz(str):
+    while true:
+        print(str)
+        output = input(">>> ")
+        if output.isdigit() == true:
+            return output
+        else:
+            print("It's not a number :( \n")
 
 #def dead(why)
 def get_random_number(level):
@@ -16,23 +27,40 @@ def get_random_number(level):
         del numbers[0::-1]
     return output
 
-
 def main():
-    set_level, level = false, 4
-    if set_level == true:
-        print("choice how many numbers you want")
-        level = input(">>> ") #str.isdigit()
+    # welcome messages
+    # while-loop:
+        # hint user to guess number
+        # compare
+        # if same:
+            # ask for again or quit
+        # else:
+            # print "1 A 2 B"
+    try_again, set_level, level = true, false, 4
+    
+    while try_again == true:
+        if set_level == true:
+            level = input_int_plz("choose how many numbers you want ?")
 
+        number = get_random_number(level)
+        guess = false
+        
+        while guess == false:
+            choice = input_int_plz("choose a number ;)")
+            guess = is_it_right(choice, number) # (1234, [5, 6, 7, 8])
+            
+            if guess == true:
+                print("would you like to try again ? (yes/N)")
+                
+                if input(">>> ") == 'yes':
+                    # try again
+                    try_again = set_level = true
+                else:
+                    print("Good bye ._./~")
+        
+        #End while -guess over
 
-    number = get_random_number(level)
-    guess = false
-    while guess == false:
-        print("your choice ;)")
-        choice = input(">>> ")
-        if choice.isdigit():
-            pass
-        else:
-            exit()
+    #End while -game over
 
 if __name__ == '__name__':
     main()
